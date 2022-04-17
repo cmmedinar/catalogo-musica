b54792b0b16ac8e3bc7609ec51f177e9cc6e29df
Cristian Medina <cristian.medina.rebolledo@ciisa.cl>
2022-04-17 19:09:41 -0400

    se agrega campo de año

--- src/controllers/CatalogController.js
+++ src/controllers/CatalogController.js
@@ -1,8 +1,8 @@
 class CatalogController {
   renderHomeWithCatalog (req, res) {
     const mockCatalog = [
-      { id: 1, artist: 'AC/DC', album: 'BACK IN BLACK' },
-      { id: 2, artist: 'NIRVANA', album: 'NEVERMIND' }
+      { id: 1, artist: 'AC/DC', album: 'BACK IN BLACK', year: '1970' },
+      { id: 2, artist: 'NIRVANA', album: 'NEVERMIND', year: '1990' }
     ]
     res.render('home', {
       articles: mockCatalog
@@ -17,7 +17,8 @@ class CatalogController {
     res.render('catalog', {
       id,
       artist: 'Este es el artista',
-      album: 'Este es el album'
+      album: 'Este es el album',
+      year: 'Este es el año de publicacion'
     })
   }
 
@@ -32,15 +33,17 @@ class CatalogController {
     res.render('catalog-form', {
       id,
       artis: 'Nombre del artista a editar',
-      album: 'Nombre del album a editar'
+      album: 'Nombre del album a editar',
+      year: 'Año de publicacion a editar'
     })
   }
 
   insertAndRenderCatalog (req, res) {
     const artist = req.body.artist
     const album = req.body.album
+    const year = req.body.year
 
-    console.log('Aquí se debería insertar el contenido en base de datos', { artist, album })
+    console.log('Aquí se debería insertar el contenido en base de datos', { artist, album, year })
 
     // TODO: Este ID debería venir como respuesta de la inserción en la base de datos
     const id = 1
@@ -52,8 +55,9 @@ class CatalogController {
     const id = req.params.id
     const artist = req.body.artist
     const album = req.body.album
+    const year = req.body.year
 
-    console.log('Aquí se debería actualizar el contenido en base de datos', { id, artist, album })
+    console.log('Aquí se debería actualizar el contenido en base de datos', { id, artist, album, year })
 
     res.redirect(`/catalog/${id}`)
   }
